1306df4b99f2cc290d07d534f6252f64291f4000
Paras Bansal <parasbansal10@gmail.com>
2018-12-23 15:49:03 +0530

    Reorder the books.

--- src/data.js
+++ src/data.js
@@ -1,35 +1,18 @@
 exports.books = [
   {
-    image: 'biology',
-    title: 'Biology',
+    image: 'science',
+    title: 'Science',
     subTitle: 'Lab Manual',
     tableData: [
       {
-        title: 'Paras Lab Manual Biology',
-        part: 'XI',
-        price: '380.00'
+        title: 'Paras Lab Manual Science',
+        part: 'IX',
+        price: '430.00'
       },
       {
-        title: 'Paras Lab Manual Biology',
-        part: 'XII',
-        price: '380.00'
-      }
-    ]
-  },
-  {
-    image: 'chemistry',
-    title: 'Chemistry',
-    subTitle: 'Lab Manual',
-    tableData: [
-      {
-        title: 'Paras Lab Manual Chemistry',
-        part: 'XI',
-        price: '330.00'
-      },
-      {
-        title: 'Paras Lab Manual Chemistry',
-        part: 'XII',
-        price: '330.00'
+        title: 'Paras Lab Manual Science',
+        part: 'X',
+        price: '460.00'
       }
     ]
   },
@@ -77,23 +60,6 @@ exports.books = [
       }
     ]
   },
-  // {
-  //   image: 'physical_education_2',
-  //   title: 'Physical Education',
-  //   subTitle: 'Lab Manual',
-  //   tableData: [
-  //     {
-  //       title: 'Paras Lab Manual Physical Education',
-  //       part: 'XI',
-  //       price: '330.00'
-  //     },
-  //     {
-  //       title: 'Paras Lab Manual Physical Education',
-  //       part: 'XII',
-  //       price: '330.00'
-  //     }
-  //   ]
-  // },
   {
     image: 'physics',
     title: 'Physics',
@@ -112,19 +78,36 @@ exports.books = [
     ]
   },
   {
-    image: 'science',
-    title: 'Science',
+    image: 'chemistry',
+    title: 'Chemistry',
     subTitle: 'Lab Manual',
     tableData: [
       {
-        title: 'Paras Lab Manual Science',
-        part: 'IX',
-        price: '430.00'
+        title: 'Paras Lab Manual Chemistry',
+        part: 'XI',
+        price: '330.00'
       },
       {
-        title: 'Paras Lab Manual Science',
-        part: 'X',
-        price: '460.00'
+        title: 'Paras Lab Manual Chemistry',
+        part: 'XII',
+        price: '330.00'
+      }
+    ]
+  },
+  {
+    image: 'biology',
+    title: 'Biology',
+    subTitle: 'Lab Manual',
+    tableData: [
+      {
+        title: 'Paras Lab Manual Biology',
+        part: 'XI',
+        price: '380.00'
+      },
+      {
+        title: 'Paras Lab Manual Biology',
+        part: 'XII',
+        price: '380.00'
       }
     ]
   },
