695e6ea7513757511a702972aca22fc15f61bc60
Paras Bansal <parasbansal10@gmail.com>
2018-12-23 15:48:37 +0530

    Add contact in the home page.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './hero/Hero'
 import Books from './books/Books'
+import Contact from '../contact/Contact'
 
 const Home = (props) => {
   return (
     <div className="Home">
       <Hero slides={props.books} />
       <Books books={props.books} />
+      <Contact />
     </div>
   );
 }
