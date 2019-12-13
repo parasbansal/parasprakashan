d1067dd83d7938d51ef7408ffe8d86f4bb1f5248
parasbansal <parasbansal10@gmail.com>
2019-12-13 16:25:22 +0530

    Added animaion on title

--- src/components/pages/home/books/Books.jsx
+++ src/components/pages/home/books/Books.jsx
@@ -20,14 +20,18 @@ const Books = (props) => {
         {renderBooks(labManuals)}
         <br />
         <br />
-        <h2 className="display-4">Books</h2>
+        <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
+          <h2 className="display-4">Books</h2>
+        </ScrollAnimation>
         {renderBooks(books)}
         <br />
         <br />
-        <h2 className="display-4">Project Work</h2>
-        <h5>
-          माध्यमिक शिक्षा परिषद उ.प्र. द्वारा लागू सतत एवं व्यापक मूल्यांकन प्रणाली के अंतर्गत 30 अंकों के प्रायोगिक एवं आन्तरिक मूल्यांकन हेतु निर्धारित पाठ्यक्रम एवं अंक विभाजन पर आधारित
-        </h5>
+        <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
+          <h2 className="display-4">Project Work</h2>
+          <h5>
+            माध्यमिक शिक्षा परिषद उ.प्र. द्वारा लागू सतत एवं व्यापक मूल्यांकन प्रणाली के अंतर्गत 30 अंकों के प्रायोगिक एवं आन्तरिक मूल्यांकन हेतु निर्धारित पाठ्यक्रम एवं अंक विभाजन पर आधारित
+          </h5>
+        </ScrollAnimation>
         {renderBooks(projectWork)}
       </div>
     </div>
